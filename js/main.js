e8597c7daf002d5219961220422bcf34ebd1d613
khilnani <nik@khilnani.org>
2018-11-21 12:59:14 -0500

    Improved short url functionality

--- js/main.js
+++ js/main.js
@@ -55,8 +55,9 @@ function update_share_link() {
     // Create a short URL
     var jqxhr = $.ajax({
         type: 'POST',
+        url: "https://khl.io/",
         data: {
-            url: "https://khl.io/?url=" + window.location.href
+            url: window.location.href
         }
     })
     jqxhr.done(function( data ) {
