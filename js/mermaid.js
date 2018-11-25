26ec4c59e0ef0e412b5fbf39bc668c6b5ba8b019
khilnani <nik@khilnani.org>
2018-11-24 19:07:38 -0500

    Fixed rendering issues with invalid mermaid uml

--- js/mermaid.js
+++ js/mermaid.js
@@ -19,19 +19,41 @@ function init_mermaid() {
 function display_mermaid(uml) {
 
     console.log('Mermaid: Updating ...');
+    try {
+        uml = uml.trim();
 
-    uml = uml.trim();
+        if(uml.toLowerCase().indexOf('@startuml') > -1) {
+            console.log('Mermaid: Skipped PlantUML');
 
-    if(uml.indexOf('sequenceDiagram') != 0 && 
-            uml.indexOf('graph') != 0 && 
-           uml.indexOf('gantt') != 0) {
-        uml = 'sequenceDiagram\n' + uml;
+            $('#mermaid').removeAttr('data-processed');
+            $('#mermaid').text('PlantUML is not Mermaid.js compatible');
+
+        } else if (uml.length < 10) {
+            console.log('Mermaid: Too short/invalid');
+
+            $('#mermaid').removeAttr('data-processed');
+            $('#mermaid').text('Is that really Mermaid.js UML?');
+
+        } else {
+
+            if(uml.indexOf('sequenceDiagram') != 0 &&
+                    uml.indexOf('graph') != 0 &&
+                   uml.indexOf('gantt') != 0) {
+                uml = 'sequenceDiagram\n' + uml;
+            }
+
+            $('#mermaid').removeAttr('data-processed');
+            $('#mermaid').text(uml);
+            $('#mermaid').addClass('mermaid');
+
+            mermaid.init(undefined, $("#mermaid"));
+            console.log('Mermaid: Updated');
+        }
     }
+    catch(e) {
+        console.log('ERROR: display_mermaid()');
+        console.log(e);
+            $('#mermaid').text('Is that really valid Mermaid.js UML?');
 
-    $('#mermaid').removeAttr('data-processed');
-    $('#mermaid').text(uml);
-    $('#mermaid').addClass('mermaid');
-
-    mermaid.init(undefined, $("#mermaid"));
-    console.log('Mermaid: Updated');
+    }
 }
